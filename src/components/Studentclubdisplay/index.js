73ddcacabb628fa438c454ba59d53e1ff86e5961
alliedgeTech <you@example.com>
2024-06-07 18:15:20 +0530

    new acco

--- src/components/Studentclubdisplay/index.js
+++ src/components/Studentclubdisplay/index.js
@@ -1,24 +1,20 @@
 import React from "react";
 import studentclubdata from "@/data/studentclub";
 import Sidebar from "@/components/Blog/Sidebar";
-import { Container, Row, Col, Table, Accordion } from "react-bootstrap";
-
-
 
 export default function Studentclub() {
   return (
     <div className="postbox__area pt-120 pb-120" id="studentclub">
       <div className="container">
-      {studentclubdata.map((std, index) => (
-        <div className="row">
-          <div className="h-100 col-xl-6 col-lg-6">
-            <div className="postbox__details-wrapper">
-              
+        {studentclubdata.map((std, index) => (
+          <div className="row mt-50">
+            <div className="h-100 col-xl-6 col-lg-6">
+              <div className="postbox__details-wrapper">
                 <div key={index}>
-                  <h1 className="it-section-title-3">{std.club}</h1>
-                  <span class="it-section-subtitle">{std.clubp}</span>
+                  <div className="three">
+                    <h1 className="it-section-title-3 mb-20">{std.club}</h1>
+                  </div>
                   <div className="it-gallery-item p-relative">
-                  
                     <div className="it-gallery-thumb">
                       <img
                         src={std.img}
@@ -29,37 +25,45 @@ export default function Studentclub() {
                     </div>
                   </div>
                 </div>
-              
+              </div>
+            </div>
+            <div className="h-100 col-xl-6 col-lg-6">
+              <div className="container d-flex align-items-center justify-content-center w-100">
+                <div className="accordion w-100" id="accordionExample">
+                  {std.accordion && std.accordion.length > 0 && (
+                    std.accordion.map((ac1, index) => (
+                      <div className="accordion-item">
+                        <h2 className="accordion-header">
+                          <button
+                            className="accordion-button"
+                            type="button"
+                            data-bs-toggle="collapse"
+                            data-bs-target={`#${index}`}
+                            aria-expanded="true"
+                            aria-controls="collapseOne"
+                          >
+                            {ac1.header}
+                          </button>
+                        </h2>
+                        <div
+                          id={`${index}`}
+                          className="accordion-collapse collapse show"
+                          data-bs-parent="#accordionExample"
+                        >
+                          <div className="accordion-body">
+                            <div
+                              dangerouslySetInnerHTML={{ __html: ac1.content }}
+                            />
+                          </div>
+                        </div>
+                      </div>
+                    ))
+                  )}
+                </div>
+              </div>
             </div>
           </div>
-          <div className="h-100 col-xl-6 col-lg-6">
-          <Container>
-      <div className="mt-100">
-        <Accordion>
-          
-            <div key={std.id}>
-              {std.accordion && std.accordion.length > 0 ? (
-                std.accordion.map((ac1, index) => (
-                  <Accordion.Item eventKey={index.toString()} key={index}>
-                    <Accordion.Header>{ac1.header}</Accordion.Header>
-                    <Accordion.Body>
-                      <div
-                        dangerouslySetInnerHTML={{ __html: ac1.content }}
-                      />
-                    </Accordion.Body>
-                  </Accordion.Item>
-                ))
-              ) : (
-                <div>No accordion data available</div>
-              )}
-            </div>
-          
-        </Accordion>
-      </div>
-    </Container>
-          </div>
-        </div>
-      ))}
+        ))}
       </div>
     </div>
   );
