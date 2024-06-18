e209912cb5611425da327ee5e73724dc06773b3d
rajpatel6604 <rpatel11032004@gmail.com>
2024-06-18 18:21:29 +0530

    done all

---
+++ src/components/Lifemu/index.js
@@ -0,0 +1,71 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div className='life-section py-5'>
+        <div className="life-heading text-center">
+            {/* <h4>Start With Your Brightest Idea</h4> */}
+            <h1>Our Latest <span>EVENTS</span></h1>
+            <h2 className='width_50 mx-auto mt-4'>Highlights: An Overview of Former University Activities</h2>
+        </div>
+        <div className="container">
+            <div className="row">
+              <div className="col-lg-6 col-12">
+                <div className="col">
+                  <div className="row-6 mt-4">
+                    <div className="img-box">
+                    <img className='img-fluid h-100' src="https://images.unsplash.com/photo-1714241759740-75a397e2a174?&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDUzfGJvOGpRS1RhRTBZfHxlbnwwfHx8fHw%3D" alt="" />
+
+                    </div>
+                  </div>
+                  <div className="row-6">
+                    <div className="row">
+                      <div className="col-6 mt-4">
+                        <div className="img-box">
+                        <img className='img-fluid h-100' src="https://images.unsplash.com/photo-1586863513779-2d0b413bd512?&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDYxfGJvOGpRS1RhRTBZfHxlbnwwfHx8fHw%3D" alt="" />
+
+                        </div>
+                      </div>
+                      <div className="col-6 mt-4">
+                        <div className="img-box">
+                        <img className='img-fluid h-100' src="https://images.unsplash.com/photo-1717781617540-9f31ba241a4e?&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDgxfGJvOGpRS1RhRTBZfHxlbnwwfHx8fHw%3D" alt="" />
+
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-6 col-12">
+              <div className="col">
+              <div className="row-6">
+                    <div className="row">
+                      <div className="col-6 mt-4">
+                        <div className="img-box">
+                        <img className='img-fluid h-100' src="https://images.unsplash.com/photo-1717620378025-f6db51a392a2?&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDEwMHxibzhqUUtUYUUwWXx8ZW58MHx8fHx8" alt="" />
+
+                        </div>
+                      </div>
+                      <div className="col-6 mt-4">
+                        <div className="img-box">
+                        <img className='img-fluid h-100' src="https://images.unsplash.com/photo-1614273845736-dc4df920c51a?&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDEwNXxibzhqUUtUYUUwWXx8ZW58MHx8fHx8" alt="" />
+
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row-6 mt-4">
+                    <div className="img-box">
+                    <img className='img-fluid h-100' src="https://images.unsplash.com/photo-1496535731694-9673400f50c9?&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDEwOHxibzhqUUtUYUUwWXx8ZW58MHx8fHx8" alt="" />
+
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default index
