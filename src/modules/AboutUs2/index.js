d7586a828f8e662b4de583e7f28cc70a737afd69
alliedgeTech <you@example.com>
2024-08-21 17:32:14 +0530

    about 2 breadcrumb

--- src/modules/AboutUs2/index.js
+++ src/modules/AboutUs2/index.js
@@ -7,7 +7,7 @@ import ScrollTop from "@/components/ScrollTop";
 export default function AboutUsModules() {
   return (
     <main>
-      <BreadCrumbs Title="About MU" />
+      <BreadCrumbs Title="About MU" Img= "/img/brand/brandimg2.jpg" />
 
       <About />
 
