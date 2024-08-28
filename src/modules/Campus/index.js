bd45705f8179f9ba074c33969fa5bc74c65ed14e
alliedgeTech <you@example.com>
2024-08-28 16:15:48 +0530

    change

--- src/modules/Campus/index.js
+++ src/modules/Campus/index.js
@@ -1,10 +1,9 @@
 import Newsletter from "@/components/Newsletter";
 import BreadCrumbs from "@/components/BreadCrumbs";
 import ScrollTop from "@/components/ScrollTop";
-import BlogArea from "@/components/Studentclubdisplay";
-// import Campusdisplay from "@/components/MediaCoverage";
+
 import Campusdisplay from "@/components/Campusdisplay";
-import Studentclub from "@/components/Studentclubdisplay";
+
 
 export default function CampusModules() {
   return (
@@ -13,7 +12,7 @@ export default function CampusModules() {
       Img= "https://res.cloudinary.com/dnjgopun8/image/upload/v1724839477/campuslife_xmpznp.jpg " />
 
       <Campusdisplay />
-      {/* <Studentclub/> */}
+      
       <Newsletter />
       <ScrollTop />
     </main>
