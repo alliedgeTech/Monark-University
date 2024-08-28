e5f2bf66966a959c4dbc6b0de30290f225063dbd
alliedgeTech <you@example.com>
2024-08-28 16:19:54 +0530

    i done

--- src/modules/Campus/index.js
+++ src/modules/Campus/index.js
@@ -1,7 +1,6 @@
 import Newsletter from "@/components/Newsletter";
 import BreadCrumbs from "@/components/BreadCrumbs";
 import ScrollTop from "@/components/ScrollTop";
-
 import Campusdisplay from "@/components/Campusdisplay";
 
 
@@ -10,9 +9,7 @@ export default function CampusModules() {
     <main>
       <BreadCrumbs Title="Campus Life"
       Img= "https://res.cloudinary.com/dnjgopun8/image/upload/v1724839477/campuslife_xmpznp.jpg " />
-
       <Campusdisplay />
-      
       <Newsletter />
       <ScrollTop />
     </main>
