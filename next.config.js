8f28592360fc17c4cebb9158cfcddc74a70bac0b
alliedgeTech <you@example.com>
2024-08-30 11:42:57 +0530

    build change

--- next.config.js
+++ next.config.js
@@ -1,27 +1,14 @@
-/** @type {import('next').NextConfig} */
-const path = require('path');
+/* @type {import('next').NextConfig} */
+const path = require("path");
 
 const nextConfig = {
+  distDir: "build",
+  output: "export",
   sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
+    includePaths: [path.join(__dirname, "styles")],
   },
-  webpack: (config, { isServer }) => {
-    // Add a rule to handle PDF files
-    config.module.rules.push({
-      test: /\.pdf$/,
-      use: [
-        {
-          loader: 'file-loader',
-          options: {
-            name: '[path][name].[ext]',
-            outputPath: 'static/pdf/',
-            publicPath: '/_next/static/pdf/',
-          },
-        },
-      ],
-    });
-
-    return config;
+  images: {
+    unoptimized: true,
   },
 };
 
